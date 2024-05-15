e41112a4253caee5d179d24b0918738df8e9f639
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-15 16:00:33 +0200

    inserito ciclo while

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 
+
+const shoppingList = ['Latte', 'Pollo', 'Fragole', 'Biscotti', 'Burro', 'Uova'];
+
+let listElement = document.getElementById("shoppingList");
+
+let i = 0;
+
+while (i < shoppingList.length) {
+      let itemElement = document.createElement("p");
+      itemElement.textContent = shoppingList[i];
+      listElement.appendChild(itemElement);
+      i++;
+}
+
+
   
 
   
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
-
+- creiamo una lista della spesa
+- 
 
    
